714c7c4b244fb59dcfb488135b14a2f274bb9746
ON3 <rachel.heimbach@essent.nl>
2016-10-16 12:30:03 +0200

    Generated chat-list component.

---
+++ src/app/chat/chat-list/chat-list.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { ChatListComponent } from './chat-list.component';
+
+describe('Component: ChatList', () => {
+  it('should create an instance', () => {
+    let component = new ChatListComponent();
+    expect(component).toBeTruthy();
+  });
+});
