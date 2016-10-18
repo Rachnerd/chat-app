d5124a2c2d489e138197837503a5c928f33eb0ff
ON3 <rachel.heimbach@essent.nl>
2016-10-18 22:34:08 +0200

    Rxjs solution

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,33 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ChatMessage } from './shared/chat-message.model';
 import { ChatService } from './shared/chat.service';
+import { Subscription } from 'rxjs/Rx';
 
 @Component({
     selector: 'ws-chat',
     templateUrl: './chat.component.html',
     styleUrls: ['./chat.component.scss']
 })
-export class ChatComponent implements OnInit {
-    messages: Array<ChatMessage> = [];
+export class ChatComponent implements OnInit, OnDestroy {
+    public messages: Array<ChatMessage> = [];
+    private messagesSubscription: Subscription;
+    private sendMessageSubscription: Subscription;
 
     constructor(private chatService: ChatService) {
-    }
-
-    ngOnInit() {
-        this.chatService
-            .getMessages()
+        this.messagesSubscription = this.chatService.messages$
             .subscribe(
                 messages => this.messages = messages,
                 error => console.error(error)
-            )
+            );
+        this.sendMessageSubscription = this.chatService.sendMessage$
+            .subscribe(
+                (message: ChatMessage) => this.messages = [message, ...this.messages],
+                (error: any) => console.error(error)
+            );
     }
 
-    onSend(content: string): void {
-        this.chatService
-            .sendMessage(new ChatMessage(content, 'ChatApp'))
-            .subscribe(
-                res => this.ngOnInit(),
-                error => console.error(error)
-            );
+    public ngOnInit(): void {
+        this.chatService.fetchMessages();
+    }
+
+    public ngOnDestroy(): void {
+        //Prevent memory leaks
+        this.messagesSubscription.unsubscribe();
+        this.sendMessageSubscription.unsubscribe();
+    }
+
+    public onSend(content: string): void {
+        this.chatService.sendMessage(new ChatMessage(content, 'ChatApp'));
     }
 }
--- src/app/chat/shared/chat.service.ts
+++ src/app/chat/shared/chat.service.ts
@@ -1,21 +1,47 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { ChatMessage } from './chat-message.model';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ChatService {
+    public messages$: Observable<Array<ChatMessage>>;
+    private messagesSubject: Subject<Array<ChatMessage>> = new Subject<Array<ChatMessage>>();
 
-  constructor(private http: Http) {}
+    public sendMessage$: Observable<ChatMessage>;
+    private sendMessageSubject: Subject<ChatMessage> = new Subject<ChatMessage>();
 
-  getMessages():Observable<any> {
-    return this.http
-        .get('http://localhost:5000/api/chat')
-        .map((res: Response) => res.json());
-  }
-  sendMessage(message: ChatMessage):Observable<Response> {
-    return this.http
-        .post('http://localhost:5000/api/chat', message);
-  }
+    constructor(private http: Http) {
+        this.messages$ = this.messagesSubject.asObservable();
+        this.sendMessage$ = this.sendMessageSubject.asObservable();
+    }
+
+    public fetchMessages(): void {
+        this.http
+            .get('http://localhost:5000/api/chat')
+            .map((res: Response) => res.json())
+            .subscribe(
+                messages => this.messagesSubject.next(messages),
+                (error: Response) => console.error(error)
+            );
+    }
+
+    public sendMessage(message: ChatMessage): void {
+        this.http
+            .post('http://localhost:5000/api/chat', message)
+            .map((res: Response) => res.json().location)
+            .switchMap(this.getMessageById)
+            .map((res: Response) => res.json())
+            .subscribe(
+                (message: ChatMessage) => this.sendMessageSubject.next(message),
+                (error: Response) => console.error(error)
+            )
+        ;
+    }
+
+    private getMessageById = (location: string) => {
+        return this.http.get('http://localhost:5000/' + location);
+    }
+
 }
